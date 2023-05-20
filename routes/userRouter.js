8627bfc7f5f3e54f296ccd13fec1509f45a6bc04
Suvrat Kumra <59862013+suvratkumra@users.noreply.github.com>
2023-05-20 11:11:18 -0400

    feat: Adding the POST/Login endpoint for users

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,11 +7,12 @@ const userRouter = Router();
 
 // use this to test if the api is working or not
 userRouter.get("/info", async (req, res) => {
+    console.log(req.session);
     res.send("This is from the user router")
 })
 
 // create a new user to the database.
-userRouter.post("/create-user", async (req, res) => {
+userRouter.post("/create", async (req, res) => {
     try {
         const { email, username, password } = req.body;
         // encrypting the password
@@ -30,6 +31,35 @@ userRouter.post("/create-user", async (req, res) => {
     }
     catch (err) {
         res.status(400).json({ status: "Rejected", error: "You are not passing right arguments." });
+    }
+
+})
+
+// login 
+userRouter.post("/login", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const userFound = await User.findOne({ "username": username });
+        if (userFound) {
+            // get the password.
+            const passwordMatches = await bcrypt.compare(password, userFound.password);
+            if (passwordMatches) {
+                // add this to session details
+                req.session.loginAuth = userFound._id;
+                // this means they are authorized to log in
+                res.status(200).json({ status: "Approved", user: userFound });
+            }
+            else {
+                res.status(400).json({ status: "Rejected", error: "Incorrect Login credentials." })
+            }
+        }
+        else {
+            res.status(400).json({ status: "Rejected", error: "Incorrect Login credentials." })
+        }
+    }
+    catch (err) {
+        res.status(400).json({ status: "Rejected", error: "Incorrect Login credentials." })
     }
 
 })
